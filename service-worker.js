ae08853e36a1b69f4e3df601f2b7c4f40cce7f36
salsabil586358-glitch <salsabil586358@gmail.com>
2026-05-23 15:14:54 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE = 'adros-v2';
+const FILES = [
+  '/LearnScienceApp/',
+  '/LearnScienceApp/index.html',
+  '/LearnScienceApp/manifest.json',
+  '/LearnScienceApp/icon-192.png',
+  '/LearnScienceApp/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e =>
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+);
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => {
+      if (r) return r;
+      return fetch(e.request).then(response => {
+        if (!response || response.status !== 200) return response;
+        const clone = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match('/LearnScienceApp/index.html'));
+    })
+  );
+});
